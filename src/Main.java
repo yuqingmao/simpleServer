1fd6a85bbe50c29b724a79dca76afbd7b79cd8ab
yuqingmao <yuqing.mao@gmail.com>
2019-08-20 18:29:24 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SocketServer server  = new SocketServer();
+        server.conn();
+    }
+}
